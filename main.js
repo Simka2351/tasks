ad45142c408125e1e287b5dd0830b401540c1992
Simka2351 <emil.asadov.2018@list.ru>
2023-04-17 02:33:55 +0400

    Push1

---
+++ main.js
@@ -0,0 +1,125 @@
+function showMessage() {
+    let number = prompt("Enter the number:");
+
+    switch (number) {
+        case "1":
+          alert("Один");
+          break;
+        case "2":
+          alert("Два");
+          break;
+        case "3":
+          alert("Три");
+          break;
+        case "4":
+          alert("Четыре");
+          break;
+        case "5":
+          alert("Пять");
+          break;
+        case "6":
+          alert("Шесть");
+          break;
+        case "7":
+          alert("Семь");
+          break;
+        case "8":
+          alert("Восемь");
+          break;
+        case "9":
+         alert("Девять");
+         break;
+       case "10":
+         alert("Десять");
+         break;
+       case "11":
+         alert("Одинадцать");
+         break;
+       case "12":
+         alert("Двенадцать");
+         break;
+       case "13":
+         alert("Тринадцать");
+         break;
+       case "14":
+         alert("Четырнадцать");
+         break;
+       case "15":
+         alert("Пятнадцать");
+         break;
+       case "16":
+         alert("Шыснацать");
+         break;
+       case "17":
+         alert("Симнацать");
+         break;
+       case "18":
+         alert("Высымнацать");
+         break;
+       case "19":
+         alert("Дэвятнацать");
+         break;
+     }
+
+    let edinitsi = ( number * 10 );
+
+    switch (edinitsi) {
+      case "1":
+        alert("Один");
+        break;
+      case "2":
+        alert("Два");
+        break;
+      case "3":
+        alert("Три");
+        break;
+      case "4":
+        alert("Четыре");
+        break;
+      case "5":
+        alert("Пять");
+        break;
+      case "6":
+        alert("Шесть");
+        break;
+      case "7":
+        alert("Семь");
+        break;
+      case "8":
+        alert("Восемь");
+        break;
+      case "9":
+        alert("Девять");
+        break;
+    }
+  
+    let desatki = ( number / 10 );
+  
+    switch (desatki) {
+      case "2":
+        alert("Двадцать");
+        break;
+      case "3":
+        alert("Тридцать");
+        break;
+      case "4":
+        alert("Сорак");
+        break;
+      case "5":
+        alert("Писят");
+        break;
+      case "6":
+        alert("Шысят");
+        break;
+      case "7":
+        alert("Семьсь");
+        break;
+      case "8":
+        alert("Восемьсь");
+        break;
+      case "9":
+        alert("Дэвяноста");
+        break;
+    }
+  }
+  showMessage();
